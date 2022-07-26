6d30dad1e3c9e7a606b1a42ef56f4994ea8b25cb
dengnh <dengninghai@shinemo.com>
2022-07-26 15:09:11 +0800

    feat：删除moke数据

--- mock/aarticleList.ts
+++ mock/aarticleList.ts
@@ -1,11 +1,3 @@
-import COVERIMG from '@/assets/images/about_me.jpg';
-import reactTemplate from './react-template.md';
-import JSON from './test.md';
-import eslint from './eslint.md';
-import react from './react.md';
-import jsModal from './js-modal.md';
-import webpackConfig from './webpack-config.md';
-
 export const list = [
   {
     id: '6',
@@ -48,78 +40,5 @@ export const list = [
     name: 'jsModal',
     desc: 'jsModal 配置',
     date: '2020-06-17',
-  },
-];
-
-export const detail = [
-  {
-    id: '6',
-    detail: {
-      name: 'webpackConfig',
-      desc: 'react + webpack + typescript + eslint 项目搭建',
-      mackdown: webpackConfig,
-      date: '2020-09-02',
-      img: COVERIMG,
-    },
-  },
-  {
-    id: '1',
-    detail: {
-      name: 'react template',
-      desc: 'react + webpack + typescript + eslint 项目搭建',
-      mackdown: reactTemplate,
-      date: '2020-06-13',
-      img: COVERIMG,
-    },
-  },
-  {
-    id: '2',
-    detail: {
-      name: 'JSON',
-      desc: 'JSON 数据测试',
-      mackdown: JSON,
-      date: '2020-06-14',
-      img: COVERIMG,
-    },
-  },
-  {
-    id: '3',
-    detail: {
-      name: 'JSON',
-      desc: 'JSON 数据测试',
-      mackdown: eslint,
-      date: '2020-06-15',
-      img: COVERIMG,
-    },
-  },
-  {
-    id: '4',
-    detail: {
-      name: 'react',
-      desc: 'react 数据测试',
-      mackdown: react,
-      date: '2020-06-16',
-      img: COVERIMG,
-    },
-  },
-  {
-    id: '5',
-    detail: {
-      name: 'react18',
-      desc: 'react18 配置',
-      mackdown: react,
-      date: '2020-06-17',
-      img: COVERIMG,
-    },
-  },
-  {
-    id: '7',
-    detail: {
-      name: 'jsModal',
-      desc: 'jsModal 配置',
-      mackdown: jsModal,
-      date: '2020-06-17',
-      img: COVERIMG,
-    },
   },
 ];
--- mock/index.ts
+++ mock/index.ts
@@ -1,6 +1,5 @@
-import { list, detail } from './aarticleList';
+import { list } from './aarticleList';
 import { tagList } from './tag';
 import { data } from './classify';
-import { comments } from './comments';
 
-export { list, detail, tagList, data, comments };
+export { list, tagList, data };
